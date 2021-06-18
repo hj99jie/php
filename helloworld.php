8cf4a02adab03575152d4686284747b4bac02087
hj99jie <hj99jie@hotmail.com>
2021-06-18 16:29:16 +0800

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Think
+ * Date: 2018/11/2
+ * Time: 11:05
+ */
+if (isset($_GET)) {
+    echo("hello world3!");
+} else {
+    echo("hello world2!");
+}
+
+
+?>
